bdd8b7a06f279c3b97c4b542c051e335c57c6a64
Tushar <139229090+tushar-fundflo@users.noreply.github.com>
2023-11-24 11:47:43 +0530

    removed appl from env dir

--- EB-ENVIRONMENT/ebenvironment.tf
+++ EB-ENVIRONMENT/ebenvironment.tf
@@ -1,9 +1,3 @@
-resource "aws_elastic_beanstalk_application" "eb_app" {
-  name        = var.eb_application_name
-  description = "${var.eb_application_name} deployment"
-}
-
-###
 resource "aws_elastic_beanstalk_environment" "eb_env" {
   name          = var.eb_environment_name
   application   = aws_elastic_beanstalk_application.eb_app.name
